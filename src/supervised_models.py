9693becbe92177056d4f71a909f1584271494d43
Daryl Fung <darylfung96@gmail.com>
2019-12-03 10:09:42 -0600

    added num samples

--- src/supervised_models.py
+++ src/supervised_models.py
@@ -20,10 +20,11 @@ model_dict = {
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--random_seed", default=42, help="Set the random state so we can reproduce the result")
+    parser.add_argument("--random_seed", default=4, help="Set the random state so we can reproduce the result")
     parser.add_argument('--dataset_type', required=True, help="automatically looks into data directory"
                                                               "example would be breast-cancer-wisconsin.data")
     parser.add_argument('--model_type', required=True, help="choose the model to train on the dataset")
+    parser.add_argument('--num_samples', type=int, help="specify the number of samples for training the model")
     arg = parser.parse_args()
 
     # get dataset
@@ -31,13 +32,17 @@ if __name__ == '__main__':
     dataset = dataset_dict[arg.dataset_type](dataset_filename, seed=arg.random_seed)
     dataset.process_dataset()
     X_train, X_test, y_train, y_test = dataset.create_train_test_dataset()
+    # if num_samples is specify, then we limit the training samples
+    if arg.num_samples:
+        X_train = X_train[:arg.num_samples]
+        y_train = y_train[:arg.num_samples]
 
     # train model
     model_initializer = model_dict[arg.model_type]
     model = model_initializer(random_state=arg.random_seed)
     model.fit(X_train, y_train)
     score = model.score(X_test, y_test)
-    print(score)
+    print(f"Accuracy: {score}")
 
 
 
